11d5ca9806d7d6b1e6edeb3c3373bf63b236ce04
tw-22 <99175142+tw-22@users.noreply.github.com>
2022-03-04 18:53:44 +0000

    Update Visualisation.py

--- Visualisation.py
+++ Visualisation.py
@@ -25,11 +25,54 @@ fig, ax = plt.subplots()
 fig.set_size_inches(5, 5)
 plt.axis('off')
 
-nx.draw_networkx(G, pos, arrows=1, edge_color='black', width=1, linewidths=1, node_size=500, node_color='C30')
+nx.draw_networkx(G, pos, arrows=1, edge_color='black', width=1, linewidths=1, node_size=500, node_color='C30', \
+                 arrowsize=18)
 nx.draw_networkx_edge_labels(G, pos, edge_labels=labels) # Add edge labels
 
 plt.savefig('TriangleInequality.png', dpi=500)
 plt.show()
 
-#%% Geodesic, Longest/Shortest Metric Path, and Longest/Shortest Network Path Visualisation
+#%% Geodesic, Longest Network Path and Shortest Network Path Visualisation
+# Blue represents longest network path; red represents shortest network path; grey represents geodesic.
+
+edges1 = [[0,1], [1,2], [2,4], [4,3], [0,5], [5,3]]
+pos1 = {0: (0.0,0.0), 1: (0.1,0.5), 2: (0.4,0.6), 3: (1.0,1.0), 4: (0.6,0.9), 5: (0.7,0.2)}
+edge_colours1 = ['C30', 'C3', 'C30', 'C30', 'C30', 'C3']
+
+G1 = nx.DiGraph()
+G1.add_edges_from(edges1)
+
+fig, ax = plt.subplots()
+fig.set_size_inches(5, 5)
+plt.axis('off')
+
+nx.draw_networkx(G1, pos1, arrows=1, edge_color=edge_colours1, width=1, linewidths=1, node_size=300, \
+                 node_color='black', font_color='black', arrowsize=20)
+    
+x = np.linspace(0,1)
+y = x
+
+plt.plot(x, y, linestyle='--', color='black', alpha=0.6) # Add geodesic
+
+plt.savefig('GeoVsNetworkPaths.png', dpi=500)
+plt.show()
+
+#%% Manhattan Space Visualisation
+
+edges2 = [[0,1], [0,2], [2,1], [0,3], [3,4], [4,5], [5,1]]
+pos2 = {0: (0.0,0.0), 1: (1.0,1.0), 2: (0.0,1.0), 3: (0.7,0.0), 4: (0.7,0.3), 5: (1.0,0.3)}
+edge_colours2 = ['C30', 'C2', 'C3', 'C2', 'C3', 'C3', 'C3']
+
+G2 = nx.DiGraph()
+G2.add_edges_from(edges2)
+
+fig, ax = plt.subplots()
+fig.set_size_inches(5, 5)
+plt.axis('off')
+
+nx.draw_networkx(G2, pos2, arrows=1, edge_color=edge_colours2, width=1, linewidths=1, node_size=500, \
+                 node_color='black', font_color='white', arrowsize=20)
+    
+plt.savefig('ManhattanSpace.png', dpi=500)
+plt.show()
 
